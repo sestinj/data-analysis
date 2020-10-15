fb59075346251e74c8cf31a7c7a99b3532452a35
sestinj <33237525+sestinj@users.noreply.github.com>
2020-10-14 21:49:48 -0400

    Just playing around with simple stats in the table

--- Firebase WebApp/public/main.js
+++ Firebase WebApp/public/main.js
@@ -12,11 +12,17 @@ function getFunctionURL(name) {
 function getAllTickers() {
     const url = "https://niyqu0sase.execute-api.us-east-1.amazonaws.com/default/get-all-data"
     $.ajax({url:url, success: (response) => {
-        console.log(response)
-        response.forEach((ticker) => {
+        console.log(response);
+        response.forEach((data) => {
             $('#ticker-list').append(`<tr>
-            <td>${ticker}</td></tr>`);
+            <td>${data[8]}</td>
+            <td>${data[2].toFixed(2)}</td>
+            <td>${data[3].toFixed(2)}</td>
+            <td>${data[4].toFixed(2)}</td>
+            <td>${(100.0*(data[4]-data[2])/data[2]).toFixed(2)}%</td>
+            </tr>`);
         });
+        $('#ticker-list').after(`<p>*Data for ${data[0][1].split(" ")[0]}</p>`);
     }});
 }
 
